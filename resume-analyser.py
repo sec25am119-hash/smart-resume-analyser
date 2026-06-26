279f6d5961213db8cdc6c5ad0d58d4ae5fe722ca
Kanishkka <sec25am119@sairamtap.edu.in>
2026-06-26 12:13:24 +0530

    Added skills detection

--- resume-analyser.py
+++ resume-analyser.py
@@ -2,5 +2,16 @@
 with open("sample_resume.txt", "r") as file:
     resume = file.read().lower()
 
-print("Resume Loaded Successfully!")
-print(resume)
+# Read skills from file
+with open("skills.txt", "r") as file:
+    skills = file.read().splitlines()
+
+print("===== SMART RESUME ANALYZER =====")
+
+print("\nResume Loaded Successfully!\n")
+
+print("Skills Found:")
+
+for skill in skills:
+    if skill.lower() in resume:
+        print("✔", skill)
