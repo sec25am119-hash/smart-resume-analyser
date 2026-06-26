1a2484f7e0269a68bb189a4c483049adab896ec0
Kanishkka <sec25am119@sairamtap.edu.in>
2026-06-26 12:08:35 +0530

    Read resume from file

--- resume-analyser.py
+++ resume-analyser.py
@@ -1,28 +1,6 @@
-print("=== Smart Resume Analyzer ===")
+# Read resume from file
+with open("sample_resume.txt", "r") as file:
+    resume = file.read().lower()
 
-resume = input("Paste your resume text:\n")
-
-skills = ["python", "java", "sql", "git", "machine learning", "communication"]
-
-resume = resume.lower()
-
-found_skills = []
-missing_skills = []
-
-for skill in skills:
-    if skill in resume:
-        found_skills.append(skill)
-    else:
-        missing_skills.append(skill)
-
-score = (len(found_skills) / len(skills)) * 100
-
-print("\nSkills Found:")
-for skill in found_skills:
-    print("✔", skill)
-
-print("\nMissing Skills:")
-for skill in missing_skills:
-    print("✘", skill)
-
-print(f"\nResume Score: {score:.1f}%")
+print("Resume Loaded Successfully!")
+print(resume)
