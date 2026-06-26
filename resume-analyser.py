476eaf2d4e42a6ee47bd2cf1478f0d9c6e8e5ed1
Kanishkka <sec25am119@sairamtap.edu.in>
2026-06-26 12:16:57 +0530

    Completed resume analyser

--- resume-analyser.py
+++ resume-analyser.py
@@ -1,17 +1,30 @@
-# Read resume from file
+# Read resume
 with open("sample_resume.txt", "r") as file:
     resume = file.read().lower()
 
-# Read skills from file
+# Read skills
 with open("skills.txt", "r") as file:
     skills = file.read().splitlines()
 
-print("===== SMART RESUME ANALYZER =====")
-
-print("\nResume Loaded Successfully!\n")
-
-print("Skills Found:")
+found_skills = []
+missing_skills = []
 
 for skill in skills:
     if skill.lower() in resume:
-        print("✔", skill)
+        found_skills.append(skill)
+    else:
+        missing_skills.append(skill)
+
+score = (len(found_skills) / len(skills)) * 100
+
+print("===== SMART RESUME ANALYZER =====")
+
+print("\nSkills Found:")
+for skill in found_skills:
+    print("✔", skill)
+
+print("\nMissing Skills:")
+for skill in missing_skills:
+    print("✘", skill)
+
+print(f"\nResume Score: {score:.0f}%")
